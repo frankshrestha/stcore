20a11ac9ded7a76deae0b2e61e232efb482ac36c
Frank Shrestha <abitofmaya@outlook.com>
2023-01-02 11:48:49 +0545

    Prioritize secrets from secret manager over environment variables over parameters

--- utils.py
+++ utils.py
@@ -1,19 +1,77 @@
+import json
+import boto3
+
 from os import getenv, uname
+
+
+def _validate_secrets(secret: dict):
+
+    status, message = (True, 'Successfully retrieved secrets.') if all(
+        secret.values()) else (False, 'Some secrets are missing.')
+
+    return {
+        'status': status,
+        'message': message,
+        'data': secret
+    }
+
+
+def _get_secrets(endpoint):
+
+    secret_id = getenv('SECRET_ID', '')
+
+    if not secret_id:
+        return {'status': False, 'message': 'SECRET_ID not found in environment.', 'data': {}}
+
+    secret_manager = boto3.client('secretsmanager')
+
+    secret = json.loads(secret_manager.get_secret_value(
+        SecretId=secret_id)['SecretString'])
+
+    secret = {
+        'user': secret['username'],
+        'password': secret['password'],
+        'host': secret['host'] if (endpoint == 'WRITER') else secret['hostreader'],
+        'port': int(secret['port']),
+        'database': secret['database']
+    }
+
+    return _validate_secrets(secret)
+
+
+def _get_env_secrets(endpoint):
+
+    secret = {
+        'user': getenv('USERNAME', 'root'),
+        'password': getenv('PASSWORD', ''),
+        'host': getenv(f'{endpoint}_HOST', 'localhost'),
+        'port': int(getenv('PORT', 3306)),
+        'database': getenv('DATABASE', '')
+    }
+
+    return _validate_secrets(secret)
 
 
 def _get_cnx_params(endpoint=None, database=None):
     on_cloud = True if ('amzn' in uname().release) else False
 
-    database = '' if on_cloud else database
+    if on_cloud:
+        resp = _get_secrets(endpoint)
 
-    database = getenv('DATABASE', database)
-    if not database:
-        raise ValueError(
-            'Provide database name or set DATABASE in the environment.')
+        if resp['status']:
+            return resp['data'].values()
 
-    host = getenv(f'{endpoint}_HOST', 'localhost')
-    port = int(getenv('PORT', 3306))
-    user = getenv('USERNAME', 'root')
-    password = getenv('PASSWORD', '')
+        print(
+            f'{resp["message"]}\n{resp["data"]}\nFalling back to environment variables.')
 
-    return user, password, host, port, database
+    resp = _get_env_secrets(endpoint)
+
+    if resp['status']:
+        return resp['data'].values()
+
+    if on_cloud:
+        raise ValueError('Required environment variables missing.')
+
+    resp['data']['database'] = database
+
+    return resp['data'].values()
